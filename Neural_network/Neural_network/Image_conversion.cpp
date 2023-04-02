82b9cd6175ee8022c626c6aa9aefbf4c770dc816
preislet <tomas.preisler1@gmail.com>
2023-04-02 12:24:52 +0200

    Added Code for conversion of image to bin arrey for neural network

---
+++ Neural_network/Neural_network/Image_conversion.cpp
@@ -0,0 +1,154 @@
+﻿#include<vector>
+#include<algorithm>
+#include<string>
+
+
+#include <Winsock2.h>
+#include <windows.h> 
+#include<gdiplus.h>
+#include<basetsd.h>
+
+#include <tchar.h>
+#include <stdio.h>
+
+
+#pragma comment(lib,"gdiplus.lib")
+#include"Image_conversion.hpp"
+
+#include <iostream>
+
+/*This function takes a path to an image file and returns a vector of vectors of RGB values that represent the pixels in the image.
+ It uses the GDI+ library to read the image file and extract the RGB values of each pixel.
+ It then stores these values in a two-dimensional vector, where each row represents a horizontal line of pixels in the image, and each column represents
+ a single pixel in that line.*/
+std::vector<std::vector<RGB>> ConvertToVector(const std::string& path)
+{
+    
+    Gdiplus::GdiplusStartupInput si;
+    ULONG_PTR token;
+    Gdiplus::GdiplusStartup(&token, &si, nullptr);
+    const std::wstring Image_path(path.begin(), path.end());
+
+    Gdiplus::Bitmap image(Image_path.c_str(), false);
+    const size_t width = image.GetWidth();
+    const size_t height = image.GetHeight();
+
+    std::vector<std::vector<RGB>> Image_vector;
+    for (size_t i = 0; i < width; i++)
+    {
+        std::vector<RGB> row_vector;
+        row_vector.reserve(width);
+        for (size_t j = 0; j < height; j++)
+        {
+            RGB rgb;
+            Gdiplus::Color argb;
+        	image.GetPixel(i, j, &argb);
+            rgb.r = argb.GetR();
+            rgb.g = argb.GetG();
+            rgb.b = argb.GetB();
+            row_vector.emplace_back(rgb);
+        }
+        Image_vector.emplace_back(row_vector);
+    }
+
+    return Image_vector;
+}
+
+/*This function takes a vector of vectors of RGB values and converts it into a binary vector of vectors, where each element is either 0 or 1,
+ depending on whether the corresponding RGB value represents a white or black pixel. It does this by iterating over each element in the input vector,
+ and checking whether its RGB values are equal to those of a white pixel (255, 255, 255).
+ If so, it sets the corresponding element in the output vector to 0; otherwise, it sets it to 1.*/
+std::vector<std::vector<int>> ConvertToBinVector(const std::vector<std::vector<RGB>>& imageRGBVector)
+{
+    std::vector<std::vector<int>> INT_vector;
+    for (int i = 0; i < imageRGBVector.size(); ++i)
+    {
+        std::vector<int> INT_vector_line;
+        for (int j = 0; j < imageRGBVector[0].size(); ++j)
+        {
+            if (imageRGBVector[i][j].r == 255 && imageRGBVector[i][j].g == 255 && imageRGBVector[i][j].b == 255) INT_vector_line.push_back(0);
+            else INT_vector_line.push_back(1);
+        }INT_vector.emplace_back(INT_vector_line);
+    }
+
+
+    std::vector<std::vector<int> >INT_vectorTranspose(INT_vector[0].size(), std::vector<int>(INT_vector.size()));
+    for (size_t i = 0; i < INT_vector.size(); ++i)
+        for (size_t j = 0; j < INT_vector[0].size(); ++j)
+
+    return INT_vectorTranspose;
+}
+
+/*(overloaded): These functions take either a path to an image file or a vector of vectors of RGB values, and convert it into a vector of integers
+ that is ready to be used as input to a compressed neural network. This is done by first calling ConvertToBinVector to convert the RGB values into binary values,
+ and then compressing the resulting binary vector by dividing it into small windows, and representing each window with a single binary value that indicates whether
+ more than half of the pixels in the window are black or white.*/
+std::vector<int> ConvertToCompressedBinaryNet_readyVector(const std::string& path)
+{
+    const std::vector<std::vector<RGB>> imageRGBVector = ConvertToVector(path);
+	std::vector<int> Img_vector = ConvertToCompressedBinaryNet_readyVector(imageRGBVector);
+    return Img_vector;
+
+}
+std::vector<int> ConvertToCompressedBinaryNet_readyVector(const std::vector<std::vector<RGB>>& imageRGBVector)
+{
+    const std::vector<std::vector<int>> INT_vector = ConvertToBinVector(imageRGBVector);
+	std::vector<int> Img_vector = ConvertToCompressedBinaryNet_readyVector(INT_vector);
+    return Img_vector;
+}
+std::vector<int> ConvertToCompressedBinaryNet_readyVector(const std::vector<std::vector<int>> &INT_vector)
+{
+    
+    std::vector<int> neuralNetReadyVector;
+	const size_t width = INT_vector[0].size();
+	const size_t height = INT_vector.size();
+
+    const size_t window_width = width / neuralNetworkImageWidth;
+    const size_t window_height = height / neuralNetworkImageWidth;
+
+
+    for(size_t windowStart_row = 0; windowStart_row < height - window_height; windowStart_row += window_height)
+    {
+	    for(size_t windowStart_column = 0; windowStart_column < width - window_width; windowStart_column += window_width)
+	    {
+            std::vector <std::vector<int>> windowVector;
+            for(size_t i = windowStart_row; i < windowStart_row+window_height; i++)
+            {
+                std::vector<int> windowVector_line;
+	            for (size_t j = windowStart_column; j < windowStart_column + window_width; j++)
+	            {
+                    
+                    windowVector_line.push_back(INT_vector[i][j]);
+	            }
+                windowVector.emplace_back(windowVector_line);
+            }
+            size_t counterOfOnes = 0;
+            for (const auto& windowLine : windowVector)
+                counterOfOnes += std::count(windowLine.begin(), windowLine.end(), 1);
+
+            if (counterOfOnes >= (window_width * window_height) / 2) neuralNetReadyVector.push_back(1);
+            else neuralNetReadyVector.push_back(0);
+            
+	    }
+    }
+    return neuralNetReadyVector;
+}
+
+/*This function takes a vector of integers that represent a compressed binary image of a handwritten digit, and prints it to the console in the form of a 28x28 grid of characters,
+ where each character represents a single pixel in the image. The character 'S' is used to represent a black pixel,
+ and a space character is used to represent a white pixel.*/
+void PrintNumber(const std::vector<int>& Img_vector)
+{
+    for (int i = 0; i < 28; ++i)
+    {
+        for (int j = 0; j < 28; ++j)
+        {
+            if (Img_vector[28 * i + j] == 0.0) std::cout << " ";
+            else std::cout << "S";
+        }
+        std::cout << std::endl;
+    }
+}
+    
+    
+   
--- Neural_network/Neural_network/Neural_network_main.cpp
+++ Neural_network/Neural_network/Neural_network_main.cpp
@@ -1,137 +1,44 @@
 ﻿#include<iostream>
 #include<vector>
-#include<algorithm>
 #include<string>
-#include<memory>
 #include<fstream>
-#include<sstream>
 
-
-#include "Neural_network.cpp"
-
-
-std::vector<std::vector<double>> read_csv(const std::string& path)
-{
-    std::ifstream CSV_File(path);
-    std::string line;
-
-    std::vector<std::vector<double>> result;
-    result.emplace_back();
-    if(CSV_File.good()) std::getline(CSV_File, line); //extract names of column
-
-    while(std::getline(CSV_File, line))
-    {
-        std::stringstream ss(line);
-        std::string tmp;
-
-        std::getline(ss, tmp, ',');
-        double tmp_int = std::stod(tmp);
-        result.back().emplace_back(tmp_int);
-
-        while(std::getline(ss,tmp, ','))
-        {
-            double tmp_int = std::stod(tmp);
-            tmp_int = (tmp_int > 0.0) ? 1.0 : 0.0;
-            result.back().emplace_back(tmp_int);
-        }
-        result.emplace_back();
-    }
-    CSV_File.close();
-    while (result.back().empty()) result.pop_back();
-    return result;
-}
-void printValues(const int label, const ptrdiff_t index, const std::vector<double>& resultValues, const std::vector<double>& inputValues, const double averageError, const bool printNumber)
-{
-    std::cout << "Expected value: " << label << std::endl;
-    std::cout << "Network output: " << index << std::endl;
-    std::cout << "Exact probability: ";
-    for (const double val : resultValues)
-        std::cout << val << " ";
-    std::cout << std::endl;
-    std::cout << "Average Error: " << averageError << std::endl;
-    std::cout << std::endl;
-
-    if (!printNumber)
-    {
-        std::cout << "=========================================================" << std::endl;
-        return;
-    }
-
-    for (int i = 0; i < 28; ++i)
-    {
-        for (int j = 0; j < 28; ++j)
-        {
-            if (inputValues[28 * i + j] == 0.0) std::cout << " ";
-            else std::cout << "S";
-        }
-        std::cout << std::endl;
-    }
-
-    std::cout << "=========================================================" << std::endl;
-}
-void testNetwork(const std::vector<std::vector<double>>& trainSamples, const std::vector<std::vector<double>>& testSamples, float eta_divider, float alpha_divider)
-{
-    const std::vector<int> topology = { 784,128,64,10 };
-    float eta = rand() / float(RAND_MAX);
-	float alpha = rand() / float(RAND_MAX);
-    eta = eta / eta_divider;
-    alpha = alpha / alpha_divider;
-    std::cout << "eta: " << eta << " alpha: " << alpha << " ";
-	
-    Net MyNetwork(topology, eta, alpha);
-
-    //train
-    for (auto trainSample : trainSamples)
-    {
-        const int label = trainSample[0];
-
-        std::vector<double> targetValues(10, 0.0);
-        targetValues[label] = 1.0;
-        trainSample.erase(trainSample.begin());
-
-        MyNetwork.FeedForward(trainSample);
-        MyNetwork.BackPropagation(targetValues);
-
-        //std::vector<double> resultValues = MyNetwork.GetResults();
-        //resultValues.pop_back();
-
-        //const auto max_element = std::max_element(resultValues.begin(), resultValues.end());
-        //const auto max_element_index = std::distance(resultValues.begin(), max_element);
-        //printValues(label, max_element_index, resultValues, trainSample, MyNetwork.AverageGetError(), false);
-    }
-
-    //test
-   
-    double correct_predictions = 0.0;
-    for (auto testSample : testSamples)
-    {
-        const int label = testSample[0];
-
-        std::vector<double> targetValues(10, 0.0);
-        targetValues[label] = 1.0;
-        testSample.erase(testSample.begin());
-        MyNetwork.FeedForward(testSample);
-        std::vector<double> resultValues = MyNetwork.GetResults();
-
-        resultValues.pop_back();
-        const auto max_element = std::max_element(resultValues.begin(), resultValues.end());
-        const auto max_element_index = std::distance(resultValues.begin(), max_element);
-        if (label == max_element_index) correct_predictions += 1.0;
-
-        //printValues(label, max_element_index, resultValues, testSample, MyNetwork.AverageGetError(), true);
-    }
-    std::cout << correct_predictions / 10000 << std::endl;
-}
+#include "Neural_network.hpp"
+#include "Image_conversion.hpp"
 
 int main()
 {
-    const auto trainSamples = read_csv("train.csv");
+
+    std::vector<std::vector<RGB>> RGB_vector = ConvertToVector("four2.jpg");
+    std::vector<std::vector<int>> INT_vector = ConvertToBinVector(RGB_vector);
+    std::vector<int> vec = ConvertToCompressedBinaryNet_readyVector(INT_vector);
+
+
+
+    
+
+
+    /*
+     *
+    const std::vector<int> topology = { 784,128,64,10 };
+    
+    //const auto trainSamples = read_csv("train.csv");
     const auto testSamples = read_csv("test.csv");
-    for (int i = 0; i < 200; i++)
+
+    for (int i = 0; i < 8; i++)
     {
-        const float eta_divider = 4;
-        const float alpha_divider = 2;
-        testNetwork(trainSamples, testSamples, eta_divider, alpha_divider);
+        const float eta = 0.033665;
+        const float alpha = 0.0889798;
+        std::cout << "eta: " << eta << " alpha: " << alpha << " ";
+
+        Net MyNetwork(topology, eta, alpha);
+        insertWeightsToNet(MyNetwork);
+        //TrainNetwork(MyNetwork, trainSamples);
+        double prop = testNetwork(MyNetwork, testSamples);
+
+        std::cout << std::endl;
     }
+    */
+	
 }
 
