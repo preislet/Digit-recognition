16ac65e9a82f4a67ae0bf7c904532368d31f40dd
preislet <tomas.preisler1@gmail.com>
2023-04-16 13:40:21 +0200

    Validation of inseretd info

--- Neural_network/Neural_network/TrainNetworkGUI.cpp
+++ Neural_network/Neural_network/TrainNetworkGUI.cpp
@@ -21,25 +21,38 @@ void TrainGUI::TrainNetworkGUI::MarshalString(String^ s, std::string& os)
 
 System::Void TrainGUI::TrainNetworkGUI::button_Train_Click(System::Object^ sender, System::EventArgs^ e)
 {
-	if (!(topologyInserted && alphaInserted && etaInserted && nameInserted && activationFunctionInserted)) return;
 	std::vector<int> topology;
+	std::string sTopology;
+
+	MarshalString(textBox_topology->Text, sTopology);
+	try
+	{
+		std::stringstream ss(sTopology);
+		std::string tmp;
+		while (getline(ss, tmp, ';'))
+			topology.emplace_back(std::stoi(tmp));
+	}
+	catch(...)
+	{
+		topologyInserted = false;
+		textBox_topology->Text = "";
+	}
+	if(topology[0] != 784 && topology.back() != 10)
+	{
+		topologyInserted = false;
+		textBox_topology->Text = "";
+	}
+
+	if (!(topologyInserted && alphaInserted && etaInserted && nameInserted && activationFunctionInserted)) return;
 
 	std::string sAlpha;
 	std::string sEta;
-	std::string sTopology;
 	MarshalString(textBox_alpha->Text, sAlpha);
 	MarshalString(textBox_eta->Text, sEta);
-	MarshalString(textBox_topology->Text, sTopology);
 	
 	ActivationFunctions activationFunction = static_cast<ActivationFunctions>(activationFunctionNum);
 	double Alpha = std::stod(sAlpha);
 	double Eta = std::stod(sEta);
-
-	std::stringstream ss(sTopology);
-	std::string tmp;
-	while (getline(ss, tmp, ';'))
-		topology.emplace_back(std::stoi(tmp));
-
 
 	CustomNet.NeuralNetUpdate(topology, Eta, Alpha, activationFunction);
 
@@ -72,3 +85,48 @@ System::Void TrainGUI::TrainNetworkGUI::check_ActivationFunction_Click(System::O
 	CheckBox^ checkBox = safe_cast<CheckBox^>(tableLayoutPanel_activationFunction->GetControlFromPosition(0, activationFunctionNum + 1));
 	checkBox->Checked = true;
 }
+System::Void TrainGUI::TrainNetworkGUI::textBox_Name_TextChanged(System::Object^ sender, System::EventArgs^ e)
+{
+	std::string name;
+	MarshalString(textBox_Name->Text, name);
+
+	if (name.size() > 0) nameInserted = true;
+	else nameInserted = false;
+}
+System::Void TrainGUI::TrainNetworkGUI::textBox_alpha_TextChanged(System::Object^ sender, System::EventArgs^ e)
+{
+	std::string sAlpha;
+	MarshalString(textBox_alpha->Text, sAlpha);
+	double alpha;
+	try
+	{
+		alpha = std::stod(sAlpha);
+		etaInserted = true;
+	}
+	catch (...)
+	{
+		etaInserted = false;
+		textBox_alpha->Text = "";
+	}
+}
+System::Void TrainGUI::TrainNetworkGUI::textBox_topology_TextChanged(System::Object^ sender, System::EventArgs^ e)
+{
+	topologyInserted = true;
+}
+System::Void TrainGUI::TrainNetworkGUI::textBox_eta_TextChanged(System::Object^ sender, System::EventArgs^ e)
+{
+	std::string sEta;
+	MarshalString(textBox_eta->Text, sEta);
+	double eta;
+	try
+	{
+		eta = std::stod(sEta);
+		etaInserted = true;
+	}
+	catch (...)
+	{
+		etaInserted = false;
+		textBox_eta->Text = "";
+	}
+}
+
